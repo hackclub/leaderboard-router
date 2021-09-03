c9c2a177b9e4bb0d05d1e1733e8dbd9c82105d93
bajpai244 <bajpaiharsh244@gmail.com>
2021-09-03 12:22:23 +0530

    feat: add test api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  console.log(req.headers);
+  res.status(200).json({ host: req.headers.host, name: "John Doe" });
+};
