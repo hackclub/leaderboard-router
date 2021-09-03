f48b0de6df10a318d8a8e564d9564b1b42563764
bajpai244 <bajpaiharsh244@gmail.com>
2021-09-03 22:13:43 +0530

    chore: add subdomain for Hack Club DUK

--- tests/create_path.js
+++ tests/create_path.js
@@ -2,7 +2,8 @@ const test = require("tape");
 const { create_path } = require("../lib/index");
 
 test.test("testign create_path utility", (t) => {
-  t.plan(6);
+  t.plan(7);
+
   t.equal("https://leaderboard.hackclub.com/club/cgc", create_path("cgc"));
 
   t.equal("https://leaderboard.hackclub.com/club/kjcmt", create_path("kjcmt"));
@@ -15,6 +16,8 @@ test.test("testign create_path utility", (t) => {
   );
 
   t.equal("https://leaderboard.hackclub.com/club/bulc", create_path("bulc"));
+
+  t.equal("https://leaderboard.hackclub.com/club/duk", create_path("duk"));
 
   t.equal("https://leaderboard.hackclub.com", create_path("some_other_domain"));
 });
