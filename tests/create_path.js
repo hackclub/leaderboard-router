49249e32eda75fa4d146dd044409f8bee408a404
bajpai244 <bajpaiharsh244@gmail.com>
2021-09-03 14:03:24 +0530

    add(sub_domain): add sub_domain kjcmt

--- tests/create_path.js
+++ tests/create_path.js
@@ -2,7 +2,10 @@ const test = require("tape");
 const { create_path } = require("../lib/index");
 
 test.test("testign create_path utility", (t) => {
-  t.plan(2);
+  t.plan(3);
   t.equal("https://leaderboard.hackclub.com/club/cgc", create_path("cgc"));
+
+  t.equal("https://leaderboard.hackclub.com/club/kjcmt", create_path("kjcmt"));
+
   t.equal("https://leaderboard.hackclub.com", create_path("some_other_domain"));
 });
