049f9b568d078f4415584b8827f037c90cd05b65
bajpai244 <bajpaiharsh244@gmail.com>
2021-09-03 16:27:03 +0530

    chore: add 2 new sub-domains, uet and byte-club

--- tests/create_path.js
+++ tests/create_path.js
@@ -2,10 +2,17 @@ const test = require("tape");
 const { create_path } = require("../lib/index");
 
 test.test("testign create_path utility", (t) => {
-  t.plan(3);
+  t.plan(5);
   t.equal("https://leaderboard.hackclub.com/club/cgc", create_path("cgc"));
 
   t.equal("https://leaderboard.hackclub.com/club/kjcmt", create_path("kjcmt"));
+
+  t.equal("https://leaderboard.hackclub.com/club/uet", create_path("uet"));
+
+  t.equal(
+    "https://leaderboard.hackclub.com/club/byte",
+    create_path("byteclub")
+  );
 
   t.equal("https://leaderboard.hackclub.com", create_path("some_other_domain"));
 });
