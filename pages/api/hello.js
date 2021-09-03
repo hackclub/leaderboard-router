9ff14a4157052fc06118e24dbe173a0bc82314a3
bajpai244 <bajpaiharsh244@gmail.com>
2021-09-03 13:03:37 +0530

    feat: release v1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,11 +3,7 @@
 export default (req, res) => {
   const sub_domain = req.headers.host.split(".")[0];
 
-  console.log("sub domain = ", sub_domain);
-
   let path = "https://leaderboard.hackclub.com";
-  //
-  res.send({ sub_domain, path });
 
-  //res.redirect(302, path);
+  res.redirect(302, path);
 };
