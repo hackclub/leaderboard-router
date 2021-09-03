e6fc5f4b5a4b10a2c47d0890133676b121eb8b87
bajpai244 <bajpaiharsh244@gmail.com>
2021-09-03 12:27:00 +0530

    feat: seperate subdomain name

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  console.log(req.headers);
+  const sub_domain = req.headers.host[0].split(".")[0];
+
   res.status(200).json({ host: req.headers.host, name: "John Doe" });
 };
